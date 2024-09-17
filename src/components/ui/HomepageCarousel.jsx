8c7f9eacfda11ac37c8e35cebe6f95865e1a5947
Saim Malik <saimmalik8397@gmail.com>
2024-09-17 20:44:13 +0500

    WV-7: Homepage design initiated added navbar menu added banner slider nextjs libraries and packages used

---
+++ src/components/ui/HomepageCarousel.jsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import "@/public/css/carousel.css";
+import Image1 from "@/public/images/slider-pic-1.png";
+import Image2 from "@/public/images/slider-pic-2.png";
+import Image3 from "@/public/images/slider-pic-3.png";
+import Autoplay from "embla-carousel-autoplay";
+import AutoScroll from "embla-carousel-auto-scroll";
+
+export function HomepageCarousel() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ loop: true, delay: 2000 }),
+  ]);
+
+  const [images] = useState([Image1, Image2, Image3]);
+
+  return (
+    <div className="embla" ref={emblaRef}>
+      <div className="embla__container">
+        {images.map((index) => (
+          <div className="embla__slide" key={index}>
+            <Image src={index} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
