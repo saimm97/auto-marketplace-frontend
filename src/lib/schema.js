9f21932340b20a7ba686b75948f0d4e4a40d4e6c
Saim Malik <saimmalik8397@gmail.com>
2024-11-26 16:57:33 +0500

    WV-7: fixed export issue for schema file

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 import { object, string, number, date } from 'yup';
 
 
-let userSchema = object({
+export let userSchema = object({
     name: string().required('Name is required'),
     // age: number().required().positive().integer(),
     email: string().required('Email is required').email(),
@@ -16,12 +16,11 @@ let userSchema = object({
     .oneOf([yup.ref('password')], 'Passwords must match')
   });
 
-let userSignInSchema = object({
+export let userSignInSchema = object({
   email: string().required('Email is required').email(),
   password: yup.string()
   .required('Password is required')
   .min(6, 'Password must be at least 6 characters'),
 });
 
-
-export default [userSchema,userSignInSchema];
+export default userSchema;
